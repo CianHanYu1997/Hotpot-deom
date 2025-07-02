d2d0291a2951a752654c00bfb3318cc3b2cdcb26
CianHan <cianhanyu@faninsights.io>
2025-07-02 23:13:45 +0800

    feat: 新增允許網域hotpot-demo.zeabur.app

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-secret-key-for-demo-only'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hotpot-demo.zeabur.app']
 
 
 # Application definition
